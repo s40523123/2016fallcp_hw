a5a83775fa5a57be7a57ab87fc81e2850ab5a1c5
s40523123 <40523123@gm.nfu.edu.tw>
2016-11-01 21:28:10 +0800

    KuanRu

--- w4.py
+++ w4.py
@@ -1,9 +1,10 @@
 '''
-mystring = "40523123"
-mystring = mystring + "test"
-print("mytring")
+mystring = "scrum-1444"
+print(mystring)
 '''
 
 thetext = input("Enter some text ")
-print ("This is what you entered:")
+print ("This is what you entered:", "test", sep="---")
 print (thetext)
+
+
